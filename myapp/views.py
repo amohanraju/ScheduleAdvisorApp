c8f2dba923529e546c67e750ecbb65f7951843ed
Ethan Ogilvie <ebo4dq@virginia.edu>
2023-02-26 19:16:08 -0500

    Added base html template containing plain nav bar and header.

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return HttpResponse("Welcome to our website!")
 
 def profile(request):
-    return HttpResponse("YOU just logged in!")
+    template = loader.get_template('myapp/home.html')
+    return HttpResponse(template.render({}, request))
+    #return HttpResponse("YOU just logged in!")
