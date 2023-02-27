24ef6ac83b66f97772c69977ea30c8f0116a8bb4
Ethan Ogilvie <ebo4dq@virginia.edu>
2023-02-26 22:09:34 -0500

    Fixed Google Login Bug so that users get redirected when not logged in.

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse("Welcome to our website!")
@@ -12,4 +13,6 @@ def profile(request):
         else:
             return HttpResponse("You just logged in! Welcome student!")
     else:
-        return HttpResponse("You are not logged in!")
+        response = redirect('/accounts/login')
+        return response
+        #return HttpResponse("You are not logged in! ")
