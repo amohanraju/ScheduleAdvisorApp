7f926f4b7d5a521b0a9352361bfc5210f09a66f2
Ethan Ogilvie <ebo4dq@virginia.edu>
2023-03-19 18:34:23 -0400

    Completely modified api to store all the classes as models within our database for easier manipulation and to lay the foundation for the shopping cart feature.

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Course(models.Model):
+    course_id = models.CharField(max_length=255)
+    course_section = models.CharField(max_length=255)
+    course_catalog_nbr = models.CharField(max_length=255)
+
+    course_subject = models.CharField(max_length=255)
+    course_mnemonic = models.CharField(max_length=255)
+
+    course_instructor = models.CharField(max_length=255)
+    course_location = models.CharField(max_length=255)
+
+    course_size = models.CharField(max_length=255)
+    course_enrollment_total = models.CharField(max_length=255)
+    course_enrollment_availability = models.CharField(max_length=255)
+    course_waitlist_total = models.CharField(max_length=255)
+    course_waitlist_cap = models.CharField(max_length=255)
+
+    course_days_of_week = models.CharField(max_length=255)
+    course_start_time = models.CharField(max_length=255)
+    course_end_time = models.CharField(max_length=255)
+
+    course_added_to_cart = models.ManyToManyField(User, related_name="courses")
+    
--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.views import generic
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
+from myapp.models import Course
 import requests
 
 class IndexView(generic.ListView):
@@ -36,6 +37,47 @@ def api_data(request):
                 'IScript_ClassSearch?institution=UVA01&term=1228&subject=%s&page=1' % class_dept
         classes = requests.get(url).json()
         #return HttpResponse(url)
+        if(len(classes) > 0):
+            for course in classes:
+                if(not Course.objects.filter(course_id= course.get("crse_id"), course_section= course.get("class_section"), course_catalog_nbr=course.get("catalog_nbr"), course_instructor = course.get("instructors")[0]['name']).exists()):
+                    course_model_instance = Course(
+                        course_id = course.get('crse_id'),
+                        course_section = course.get('class_section'),
+                        course_catalog_nbr = course.get('catalog_nbr'),
+
+                        course_subject = course.get('descr'),
+                        course_mnemonic = course.get('subject'),
+
+                        course_instructor = course.get("instructors")[0]['name'],
+                        course_location = course.get("meetings")[0]['facility_descr'],
+
+                        course_size = course.get('class_capacity'),
+                        course_enrollment_total = course.get('enrollment_total'),
+                        course_enrollment_availability = course.get('enrollment_available') ,
+                        course_waitlist_total = course.get('wait_tot'),
+                        course_waitlist_cap = course.get('wait_cap'),
+
+                        course_days_of_week = course.get("meetings")[0]['days'],
+                        course_start_time = course.get("meetings")[0]['start_time'],
+                        course_end_time = course.get("meetings")[0]['end_time'],
+
+                        course_added_to_cart = []
+                    )
+                    course_model_instance.save()
+                    #print('cat')
+
+                #For updating info if users join / get off waitlist and as enrollment size changes
+                specific_course = Course.objects.get(course_id= course.get("crse_id"), course_section= course.get("class_section"), course_catalog_nbr=course.get("catalog_nbr"), course_instructor = course.get("instructors")[0]['name'])    
+                if(specific_course.course_enrollment_total != course.get('enrollment_total') or 
+                   specific_course.course_enrollment_availability != course.get('enrollment_available') or
+                   specific_course.course_waitlist_total != course.get('wait_tot') or 
+                   specific_course.course_waitlist_cap != course.get('wait_cap')):
+                        specific_course.course_enrollment_total = course.get('enrollment_total'),
+                        specific_course.course_enrollment_availability = course.get('enrollment_available') ,
+                        specific_course.course_waitlist_total = course.get('wait_tot'),
+                        specific_course.course_waitlist_cap = course.get('wait_cap'),
+                        specific_course.save()
+
         return render(request, 'myapp/courses.html', {'classes' : classes})
     else:
         return HttpResponseRedirect('accounts/profile/browse_courses')
