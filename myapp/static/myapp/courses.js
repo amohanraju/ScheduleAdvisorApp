4f1832c5e7b388082954bc72d4682c1446ac14c2
Adarsh Mohanraju <hmz4kf@virginia.edu>
2023-03-19 20:35:59 -0400

    updates

---
+++ myapp/static/myapp/courses.js
@@ -0,0 +1,125 @@
+$(document).ready(function () {
+    $("#classes").change(function () {
+        var val = $(this).val();
+        if (val == "AAS") {
+            $("#mnemonic").html("<option value='test'> AAS </option>");
+        } 
+        else if (val == "ACCT") {
+            $("#mnemonic").html("<option value='test'> ACCT </option>");
+        }
+        else if (val == "AIRS") {
+            $("#mnemonic").html("<option value='test'> AIRS </option>");
+        } 
+        else if (val == "ALAR") {
+            $("#mnemonic").html("<option value='test'> ALAR </option>");
+        }  
+        else if (val == "AMST") {
+            $("#mnemonic").html("<option value='test'> AMST </option>");
+        }  
+        else if (val == "ANTH") {
+            $("#mnemonic").html("<option value='test'> ANTH </option>");
+        }  
+        else if (val == "APMA") {
+            $("#mnemonic").html("<option value='test'> APMA </option>");
+        }  
+        else if (val == "ARAB") {
+            $("#mnemonic").html("<option value='test'> ARAB </option>");
+        }  
+        else if (val == "ARAD") {
+            $("#mnemonic").html("<option value='test'> ARAD </option>");
+        }  
+        else if (val == "ARAH") {
+            $("#mnemonic").html("<option value='test'> ARAH </option>");
+        }  
+        else if (val == "ARCH") {
+            $("#mnemonic").html("<option value='test'> ARCH </option>");
+        }  
+        else if (val == "ARCY") {
+            $("#mnemonic").html("<option value='test'> ARCY </option>");
+        }  
+        else if (val == "ARH") {
+            $("#mnemonic").html("<option value='test'> ARH </option>");
+        }  
+        else if (val == "ARTH") {
+            $("#mnemonic").html("<option value='test'> ARTH </option>");
+        }  
+        else if (val == "ARTR") {
+            $("#mnemonic").html("<option value='test'> ARTR </option>");
+        }  
+        else if (val == "ARTS") {
+            $("#mnemonic").html("<option value='test'> ARTS </option>");
+        }  
+        else if (val == "ASL") {
+            $("#mnemonic").html("<option value='test'> ASL </option>");
+        }  
+        else if (val == "ASTR") {
+            $("#mnemonic").html("<option value='test'> ASTR </option>");
+        }  
+        else if (val == "BIMS") {
+            $("#mnemonic").html("<option value='test'> BIMS </option>");
+        }  
+        else if (val == "BIOC") {
+            $("#mnemonic").html("<option value='test'> BIOC </option>");
+        }  
+        else if (val == "BIOL") {
+            $("#mnemonic").html("<option value='test'> BIOL </option>");
+        }  
+        else if (val == "BIOP") {
+            $("#mnemonic").html("<option value='test'> BIOP </option>");
+        }  
+        else if (val == "BME") {
+            $("#mnemonic").html("<option value='test'> BME </option>");
+        }  
+        else if (val == "BUS") {
+            $("#mnemonic").html("<option value='test'> BUS </option>");
+        }  
+        else if (val == "CASS") {
+            $("#mnemonic").html("<option value='test'> CASS </option>");
+        }  
+        else if (val == "CE") {
+            $("#mnemonic").html("<option value='test'> CE </option>");
+        }  
+        else if (val == "CELL") {
+            $("#mnemonic").html("<option value='test'> CELL </option>");
+        }  
+        else if (val == "CHE") {
+            $("#mnemonic").html("<option value='test'> CHE </option>");
+        }  
+        else if (val == "CHEM") {
+            $("#mnemonic").html("<option value='test'> CHEM </option>");
+        }  
+        else if (val == "CHIN") {
+            $("#mnemonic").html("<option value='test'> CHIN </option>");
+        }  
+        else if (val == "CHTR") {
+            $("#mnemonic").html("<option value='test'> CHTR </option>");
+        }  
+        else if (val == "CLAS") {
+            $("#mnemonic").html("<option value='test'> CLAS </option>");
+        }  
+        else if (val == "COGS") {
+            $("#mnemonic").html("<option value='test'> COGS </option>");
+        }  
+        else if (val == "COMM") {
+            $("#mnemonic").html("<option value='test'> COMM </option>");
+        }  
+        else if (val == "CONC") {
+            $("#mnemonic").html("<option value='test'> CONC </option>");
+        }  
+        else if (val == "CPE") {
+            $("#mnemonic").html("<option value='test'> CPE </option>");
+        }  
+        else if (val == "CREO") {
+            $("#mnemonic").html("<option value='test'> CREO </option>");
+        }  
+        else if (val == "CS") {
+            $("#mnemonic").html("<option value='test'> CS </option>");
+        }  
+        else if (val == "DANC") {
+            $("#mnemonic").html("<option value='test'> DANC </option>");
+        }  
+        else if (val == "DEM") {
+            $("#mnemonic").html("<option value='test'> DEM </option>");
+        }   
+    });
+});
