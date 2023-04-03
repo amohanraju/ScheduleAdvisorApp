8c232a991bd7262b4fbcce9ae63ff1b2e2206994
Ethan Ogilvie <ebo4dq@virginia.edu>
2023-04-03 16:04:52 -0400

    Reverted static_url change.

--- mysite/settings.py
+++ mysite/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/myapp/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
